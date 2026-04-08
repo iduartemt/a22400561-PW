6dbd5dc0f3a966689314ac2dbbfdc91b912ffd82
Duarte Martins <iduartemt05.04@gmail.com>
2026-04-08 20:36:46 +0000

    Implementar views e templates para exibir UCs e curso: home com plano curricular, detalhes do curso e detalhes das UCs

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('curso/', views.curso_detail, name='curso_detail'),
+    path('uc/<int:uc_id>/', views.uc_detail, name='uc_detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Licenciatura, UnidadeCurricular
 
 def home(request):
-    return render(request, 'portfolio/home.html')
+    licenciatura = Licenciatura.objects.first()
+    ucs_por_ano = {}
+    for uc in UnidadeCurricular.objects.all().order_by('ano', 'semestre', 'nome'):
+        ano = uc.ano
+        if ano not in ucs_por_ano:
+            ucs_por_ano[ano] = []
+        ucs_por_ano[ano].append(uc)
+    
+    context = {
+        'licenciatura': licenciatura,
+        'ucs_por_ano': ucs_por_ano,
+    }
+    return render(request, 'portfolio/home.html', context)
+
+def curso_detail(request):
+    licenciatura = get_object_or_404(Licenciatura)
+    context = {
+        'licenciatura': licenciatura,
+    }
+    return render(request, 'portfolio/curso_detail.html', context)
+
+def uc_detail(request, uc_id):
+    uc = get_object_or_404(UnidadeCurricular, pk=uc_id)
+    context = {
+        'uc': uc,
+    }
+    return render(request, 'portfolio/uc_detail.html', context)
