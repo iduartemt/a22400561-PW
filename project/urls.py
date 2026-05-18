0ccb5d5a7ba9d3f50f4b502305a7573a8243a97b
Duarte Martins <iduartemt05.04@gmail.com>
2026-05-18 04:03:07 +0100

    feat: integrate django-allauth urls in project configurations

--- project/urls.py
+++ project/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('artigos/', include('artigos.urls')),
 ]
 
