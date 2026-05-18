50c306ca0c00efa73ce7fd8cb77aafd58f7f416e
Duarte Martins <iduartemt05.04@gmail.com>
2026-05-18 01:25:42 +0100

    fix: update ALLOWED_HOSTS for production

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = "django-insecure-k)8+q+ua6u0ljy=ma47yigv!sf%2(h8mr_ysiu)op^xejz7=z_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['duartemartins-a22400561.pw.deisi.ulusofona.pt', '127.0.0.1', 'localhost']
 
 
 # Application definition
